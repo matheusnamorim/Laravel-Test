8fd12d99dd126cbc83b10a75cf9eb740e28eb2e1
Matheus Amorim <matheusnavesamorim@gmail.com>
2024-04-04 20:00:41 -0300

    Feat: updating and deleting books

--- back-end/app/Http/Controllers/autoresController.php
+++ back-end/app/Http/Controllers/autoresController.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 class autoresController extends Controller
 {
 
-    public function __construct(Autores $autores)
-    {
-    }
-
     public function index() 
     {
         return Autores::all();
@@ -26,17 +22,13 @@ class autoresController extends Controller
     {
         $autoresModel = Autores::find($autores);
         if ($autoresModel === null) {
-            return response()->json(['message' => 'Autor não encontrado'], status: 404);
+            return response()->json(['error' => true, 'message' => 'Autor não encontrado'], status: 404);
         }
         return $autoresModel;
     }
 
     public function update(Request $request, int $autores)
     {
-        // $autoresUpdate = Autores::find($autores);
-        // $autoresUpdate->fill($request->all());
-        // $autoresUpdate->save();
-
         Autores::where('id', $autores)->update($request->all());
 
         return Autores::find($autores);
